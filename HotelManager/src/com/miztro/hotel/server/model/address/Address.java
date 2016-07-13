04f7c41b9dca58877c755f7c87f43824c6276680
Stephen Wilson <stephenwilson1984@outlook.com>
2016-07-13 12:21:26 +0100

    Update test

--- HotelManager/src/com/miztro/hotel/server/model/address/Address.java
+++ HotelManager/src/com/miztro/hotel/server/model/address/Address.java
@@ -1,5 +1,5 @@
 package com.miztro.hotel.server.model.address;
 
 public class Address {
-
+	private String Id;
 }
--- HotelManager/src/com/miztro/hotel/server/model/guests/Guest.java
+++ HotelManager/src/com/miztro/hotel/server/model/guests/Guest.java
@@ -1,5 +1,5 @@
 package com.miztro.hotel.server.model.guests;
 
 public class Guest {
-
+	private String Id;
 }
